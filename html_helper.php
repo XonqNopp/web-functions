4d3cf5a5a23c1abcd47fdf7ab26d58979945a8a5
XonqNopp <xonqnoppp@gmail.com>
2025-09-03 07:40:46 +0200

    html: allow to choose html lang value

--- html_helper.php
+++ html_helper.php
@@ -59,17 +59,17 @@ class HtmlHelper extends MyHelper {
         }
     //
         // HTML tag
-        private function htmltag() {
+        private function htmltag($language="en") {
             $back = "<html";
-            $back .= ' lang="en"';
+            $back .= ' lang="' . $language . '"';
             $back .= ">\n";
-            $this->logger->trace("htmltag()");
+            $this->logger->trace("htmltag($language)");
             return $back;
         }
     //
         // Init HTML: open doctype and html
-        private function init() {
-            $this->logger->trace("init()");
+        private function init($language="en") {
+            $this->logger->trace("init($language)");
 
             if($this->isOpened) {
                 $this->logger->info("init(): already opened HTML");
@@ -78,7 +78,7 @@ class HtmlHelper extends MyHelper {
 
             $foetus = "";
             $foetus .= $this->doctypetag();
-            $foetus .= $this->htmltag();
+            $foetus .= $this->htmltag($language);
             $foetus .= "<!-- Hey, why do you check the source code? ;-) -->\n";
             echo $foetus;
 
@@ -268,9 +268,9 @@ class HtmlHelper extends MyHelper {
         }
     //
         // init HTML+body
-        public function hotBooty() {
-            $this->logger->trace("hotBooty()");
-            $this->init();
+        public function hotBooty($language="en") {
+            $this->logger->trace("hotBooty($language)");
+            $this->init($language);
             $this->makeHead();
             $this->decapite();
         }
